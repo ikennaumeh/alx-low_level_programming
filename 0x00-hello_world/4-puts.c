71b618681a2f38e5e24d1c076d13d8ad48704ae1
ikennaumeh <kennaumeh@gmail.com>
2022-03-11 12:39:47 -0800

    updares

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - entry point
- * @void: no arguments
- * Description: the program starts and ends here.
- * Return: value 0 to exit
- */
+ *main - entry point
+ *@void: no arguments
+ *Description: the program starts and ends here.
+ *Return: value 0 to exit
+*/
 int main(void)
 {
-puts("\"programming is like building multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multlingual puzzle");
+return (0);
 }
