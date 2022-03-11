c9e28b436a79d777a8f6d1034d00feb969fa80ad
ikennaumeh <kennaumeh@gmail.com>
2022-03-11 12:46:26 -0800

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- *main - entry point
- *@void: no arguments
- *Description: the program starts and ends here.
- *Return: value 0 to exit
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multlingual puzzle");
-return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
