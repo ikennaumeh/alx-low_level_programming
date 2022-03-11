fa4a1afd28e3457676c48835af64634f6fb52e6c
ikennaumeh <kennaumeh@gmail.com>
2022-03-11 12:29:47 -0800

    updates and finished

---
+++ 0x00-hello_world/5-prints.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main -entry point
+* @void: no arguments
+* Description: the program starts and ends here.
+* Return: value 0 to exit
+*/
+int main(void)
+{
+  printf("with proper grammer, but the outcome is piece of art,\n");
+  return (0);
+}
